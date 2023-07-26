bf8a48f08e58eca3c18458dd1e996fe73a7b14a2
Rahul <247webs@gmail.com>
2023-07-26 17:08:02 +0530

    Entity Merger

---
+++ src/DataFixtures/LoadUserData.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+/**
+ * @Embedded\Embedded
+ */
+class LoadUserData extends Fixture
+{
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('neverovski');
+        $password = $this->encoder->encodePassword($user, 'Security123!');
+        $user->setRoles([User::ROLE_ADMIN]);
+        $user->setPassword($password);
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+
+}
